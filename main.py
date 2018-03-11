9801383f914c07fa8b57a3a641897960870710b9
Lachi B <34084406+lachibal2@users.noreply.github.com>
2018-03-11 09:59:01 -0400

    functional

--- main.py
+++ main.py
@@ -10,15 +10,5 @@ app = Flask(__name__, static_folder="web", static_url_path='')
 def root():
 	return app.send_static_file("index.html")
 
-
-@app.route('/homework')
-def homework():
-	return app.send_static_file("homework.html")
-
-@app.route('/nutrition')
-def nutrition():
-	return app.send_static_file("nutrition.html")
-
-
 if __name__ == "__main__":
 	app.run(port=PORT, debug=DEBUG)
